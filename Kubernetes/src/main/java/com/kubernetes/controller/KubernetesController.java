4be6be3c1beb1f5b96dcec2dff15c9464f97d1ab
Devang Mulye <devangmulye@gmail.com>
2026-03-07 03:01:19 +0530

    Add helper methods and other functionality

---
+++ Kubernetes/src/main/java/com/kubernetes/controller/KubernetesController.java
@@ -0,0 +1,14 @@
+package com.kubernetes.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KubernetesController {
+
+    @GetMapping("/test")
+    public String testHello() {
+        return "Hello, World!";
+    }
+}
